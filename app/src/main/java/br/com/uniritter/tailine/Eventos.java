aa304b63f49d3eb10040c22407ce4c30a5dcfd4c
Renato-0 <79981158+Renato-0@users.noreply.github.com>
2021-06-16 07:48:34 -0300

    Add files via upload

--- app/src/main/java/br/com/uniritter/tailine/Eventos.java
+++ app/src/main/java/br/com/uniritter/tailine/Eventos.java
@@ -2,18 +2,27 @@ package br.com.uniritter.tailine;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CalendarView;
 import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import org.jetbrains.annotations.NotNull;
 
 import br.com.uniritter.tailine.services.FirebaseServices;
 
@@ -21,14 +30,15 @@ public class Eventos extends AppCompatActivity {
 
     private Button maiores, menores, share;
     private TextView frequentadores, eventos;
-    private ImageView logo;
+    private CalendarView logo;
     private CheckBox checkBox;
 
     private Boolean presenca;
 
     private FirebaseFirestore db = FirebaseServices.getFirebaseFirestoreInstance();
     private final FirebaseUser user = FirebaseServices.getFirebaseAuthInstance().getCurrentUser();
-
+    private static final String TAG = "CadastroEvento";
+    private static final String TAG1 = "Calendario";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,11 +49,26 @@ public class Eventos extends AppCompatActivity {
         maiores = (Button) findViewById(R.id.btn_maiores);
         checkBox = findViewById(R.id.checkBox);
         share = (Button) findViewById(R.id.btnShare);
-        logo = (ImageView) findViewById(R.id.img_eventos);
+        logo = (CalendarView) findViewById(R.id.cv_eventos);
 
-        logo.setOnClickListener(new View.OnClickListener() {
+        //logo.setOnClickListener(new View.OnClickListener() {
+        //    @Override
+        //    public void onClick(View v){ calendario(); }
+        //});
+
+        logo.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
-            public void onClick(View v){ calendario(); }
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                String date = dayOfMonth + "/" + (month + 1) + "/" + year;//da uma olhada se não precisa mais um.
+                Log.d(TAG1, "onSelectDayChange: dd/mm/yyyy: " + date);
+
+
+
+
+                Intent intent = new Intent(Eventos.this, Calendario.class);
+                intent.putExtra("date", date);
+                startActivity(intent);
+            }
         });
 
         share.setOnClickListener(new View.OnClickListener() {
@@ -93,4 +118,27 @@ public class Eventos extends AppCompatActivity {
         Intent shareIntent = Intent.createChooser(sendIntent, null);
         startActivity(shareIntent);
     }
+
+    public void proxEventos(View view) {
+        db.collection("eventos")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull @NotNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            Log.d(TAG, "Erro ao abrir", task.getException());
+                        }
+                    }
+
+
+                });
+
+
+    }
+
+
 }
